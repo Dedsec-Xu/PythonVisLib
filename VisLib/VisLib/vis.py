f9e969fabcec958296f88f1dcfb09ffb49b14b2f
Liangdong Xu <liangdox@andrew.cmu.edu>
2022-11-10 09:45:23 -0500

    finished multiple image

--- VisLib/VisLib/vis.py
+++ VisLib/VisLib/vis.py
@@ -2,6 +2,11 @@ import random
 import string
 import imageio
 import webbrowser
+import json
+import matplotlib
+import numpy
+
+import PIL
 
 
 
@@ -12,13 +17,147 @@ def generate_random_handler(randomlength):
 
 
 
-class ImageHandler:
-   def __init__(self, img):
-      self.handle = generate_random_handler(20)
-      imageio.imwrite("./static/image/"+self.handle+".png", img)
-      webbrowser.open('http://127.0.0.1:8000/show/'+self.handle)
 
 
-def func(im2):
-   ImageHandler(im2)
-   return
+class PageJson():
+   def __init__(self, **kwargs):
+      self.imglist = kwargs['imglist']
+      # self.imgcnt = kwargs['imgcnt']
+      self.titlelist = kwargs['titlelist']
+      self.mode = kwargs['mode']
+      self.layout= kwargs['layout']
+
+      self.rectlist = kwargs['rectlist']
+
+   def saveJson(self,str):
+      js = json.dumps(self.__dict__)
+      print(js)
+      fh = open(str, 'w')
+      fh.write(js)
+      fh.close()
+
+class DrawJson():
+   def __init__(self, **kwargs):
+      self.rect = kwargs['rect']
+      self.recttag = kwargs['recttag']
+      self.dot = kwargs['dot']
+      self.dottag = kwargs['dottag']
+      self.line = kwargs['line']
+      self.dottag = kwargs['dottag']
+      self.style = kwargs['style']
+
+   def saveJson(self,str):
+      js = json.dumps(self.__dict__)
+      print(js)
+      fh = open(str, 'w')
+      fh.write(js)
+      fh.close()
+
+class MakeRect():
+   def __init__(self, xy, width, height, **kwargs):
+      self.x = xy[0]
+      self.y = xy[1]
+      self.width = width
+      self.height = height
+      if 'label' in kwargs:
+         self.label = kwargs['label']
+      else:
+         self.label = ''
+      if 'edgecolor' in kwargs:
+         self.edgecolor = matplotlib.colors.to_hex(kwargs['edgecolor'])
+      else:
+         self.edgecolor = matplotlib.colors.to_hex('r')
+
+      if 'linewidth' in kwargs:
+         self.linewidth = kwargs['linewidth']
+      else:
+         self.linewidth = "1"
+
+   def makejson(self):
+      return json.dumps(self.__dict__)
+
+# class Rects():
+#    def __init__(self):
+#
+#    def append(self,id:int, r:MakeRect):
+
+
+
+
+class ImageHandler():
+   def __init__(self, *args, **kwargs):
+      self.handles = []
+      self.imglist = []
+      self.titlelist = []
+      self.rectlist = []
+      # print(kwargs)
+
+      self.checkInput(*args, **kwargs)
+      self.jsonhandle = generate_random_handler(20)
+      for img in args:
+         handle = generate_random_handler(20)
+         imageio.imwrite("./static/image/"+handle+".png", img)
+         # webbrowser.open('http://127.0.0.1:8000/show/'+handle)
+         self.handles.append(handle)
+         self.imglist.append(handle)
+      if kwargs['caller'] == 'showimg':
+         if 'title' in kwargs:
+            self.titlelist.append(kwargs['title'])
+         else:
+            self.titlelist.append('')
+      if kwargs['caller'] == 'showimgs':
+         if 'title' in kwargs:
+            self.titlelist = kwargs['title'][:]
+         else:
+            self.titlelist = [''] * len(args)
+      if 'rect' in kwargs:
+         self.rectlist = json.loads(json.dumps(kwargs['rect'][:]))
+
+      PageJson(imglist = self.imglist, titlelist = self.titlelist, mode = '1', layout = '1',rectlist=self.rectlist).saveJson("./static/json/"+self.jsonhandle+".json")
+
+      webbrowser.open('http://127.0.0.1:8000/showjs/' + self.jsonhandle)
+       # 'http://127.0.0.1:8000/imagesource/json/GMJedBqSVIE5vUtgOLRu.json'
+
+
+   def checkInput(self, *args, **kwargs):
+      if len(args) == 0 :
+         raise RuntimeError('no input')
+      if kwargs['caller'] == 'showimg':
+         # print('showimg')
+         if 'title' in kwargs:
+            if not isinstance(kwargs['title'],str):
+               raise RuntimeError('title should be a string')
+      elif kwargs['caller'] == 'showimgs':
+         # print('showimgs')
+         if 'title' in kwargs:
+            if not isinstance(kwargs['title'], list):
+               raise RuntimeError('title should be a list')
+            elif not all(isinstance(n, str) for n in kwargs['title']):
+
+               raise RuntimeError('title should be a list of strings')
+            elif len(kwargs['title'])!=len(args):
+               print('title list mismatch with images')
+
+
+
+
+def showimg(img, **kwargs):
+   print(kwargs)
+   x = ImageHandler(img, **kwargs, caller = 'showimg')
+   return x.jsonhandle
+
+
+def showimgs(*args, **kwargs):
+   handles = []
+   x = ImageHandler(*args, **kwargs, caller = 'showimgs')
+   # for img in args:
+   #    x = ImageHandler(img)
+   #    handles.append(x.handle)
+   return x.jsonhandle
+
+# def showimgs(**kwargs):
+#    handles = []
+#    for img in args:
+#       x = ImageHandler(img,1)
+#       handles.append(x.handle)
+#    return handles
--- VisLib/test.py
+++ VisLib/test.py
@@ -5,22 +5,69 @@ from PIL import Image
 import cv2
 from VisLib import vis
 
-im = array(Image.open('1.png').convert('L'))
+im1 = Image.open('1.png')
+im = array(im1.convert('L'))
 H = array([[1.4,0.05,-100],[0.05,1.5,-100],[0,0,1]])
 im2 = ndimage.affine_transform(im, H[:2,:2],(H[0,2],H[1,2]))
 
-vis.func(im)
-vis.func(im2)
+im3 = Image.open('2.png')
+H = array([[2,0.05,-100],[0.03,1.5,-100],[0,0,2]])
+
+im = array(im3.convert('L'))
+im4 = ndimage.affine_transform(im, H[:2,:2],(H[0,2],H[1,2]))
+# vis.showimg(im1)
+# vis.showimg(im1,title = '12')
+
+# vis.showimgs(im1,im2,im1,im2)
+titles = ['cat1','cat2','dog','dog2']
+rect1 = vis.MakeRect([67,40],147,147,linewidth = 3).makejson()
+rect2 = vis.MakeRect([87,130],93,18,edgecolor = 'w').makejson()
+rect3 = vis.MakeRect([56,63],189,188,edgecolor = 'g',linewidth = 3).makejson()
+rect4 = vis.MakeRect([120,27],66,55,edgecolor = 'cyan',linewidth = 1).makejson()
+rect5 = vis.MakeRect([62,49],152,114,edgecolor = 'm',linewidth = 5).makejson()
+rects1 = []
+rects1.append(rect1)
+rects1.append(rect2)
+rects2 = []
+rects2.append(rect3)
+rects3 = []
+rects3.append(rect4)
+rects4 = []
+rects4.append(rect5)
+rectss=[]
+rectss.append(rects1)
+rectss.append(rects2)
+rectss.append(rects3)
+rectss.append(rects4)
+# print(rectss)
+
+# vis.showimgs(im1,im2,im1,im2, title = titles)
+# vis.showimgs(im1,im2,im3,im4, title = titles, rect = rectss)
+
+
+# titles2 = titles + titles + titles + titles
+# vis.showimgs(im1,im2,im3,im4,im1,im2,im3,im4,im1,im2,im3,im4,im1,im2,im3,im4, title = titles)
+
+# vis.showimgs(im1,im2,im2,im2)
+vis.showimg(im2,title = 'cat')
 
 
 
 
-# im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2RGB)
-# im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2RGB)
+im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 
 
-subplot(121)
-imshow(im)
-subplot(122)
-imshow(im2)
-show()
+# subplot(121)
+# imshow(im)
+# subplot(122)
+# imshow(im2)
+# show()
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+rect = plt.Rectangle((0,0),100,100,fill=False, edgecolor = 'red',linewidth=1)
+ax.add_patch(rect)
+plt.imshow(im1) # 图像数组
+plt.show()
